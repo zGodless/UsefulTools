7bcf6d1dfb46530548b47b6eece91d54d8a0fda0
zGodless <zyl@cnsb.top>
2019-01-29 08:53:11 +0800

    提交添加form

--- UsefulTools/Form1.Designer.cs
+++ UsefulTools/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnJSONCheck = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnJSONCheck);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 285);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnJSONCheck
+            // 
+            this.btnJSONCheck.Location = new System.Drawing.Point(12, 12);
+            this.btnJSONCheck.Name = "btnJSONCheck";
+            this.btnJSONCheck.Size = new System.Drawing.Size(109, 52);
+            this.btnJSONCheck.TabIndex = 0;
+            this.btnJSONCheck.Text = "JSON校验";
+            this.btnJSONCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 285);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnJSONCheck;
     }
 }
 
